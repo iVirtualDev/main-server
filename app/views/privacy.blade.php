3ede7d434be67ce28a572e47a689c4e86e2a4365
Nick Comer <nick@comer.io>
2013-08-16 12:23:02 -0400

    beginning work on the privacy policy

--- app/views/privacy.blade.php
+++ app/views/privacy.blade.php
@@ -5,10 +5,10 @@
 		<div class="large-12 columns">
 			<div class="panel">
 				<header>
-					<h3>Your Second Phone Privacy Policy</h3>
+					<h2>Your Second Phone Privacy Policy</h2>
 				</header>
 				<div class="panel_body">
-					<h4>Our Approach</h4>
+					<h3>Our Approach</h3>
 					<p>In light of recent privacy encroachments made by various governments around the world, Your Second Phone is taking the "DuckDuckGo" approach to privacy which is; we simply just don't collect any unique data on our users, so there is no data to be requested by prying entites.</p>
 				</div>
 			</div>
