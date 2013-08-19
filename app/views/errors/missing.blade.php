c7cc3b9926c77e98d72ce7a04364edc2bbd37fbc
Nick Comer <nick@comer.io>
2013-08-19 15:07:15 -0400

    the 404 view

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('main')
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="panel error">
+				<header>
+					<h3><i class="icon-question-sign"></i>&nbsp;404 Not Found</h3>
+				</header>
+				<div class="panel_body">
+					<p>The page you are looking for is not here anymore.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
