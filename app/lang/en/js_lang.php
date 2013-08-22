a7dd5553bbf7ed47237d93e0bbb62dc5cca3c58a
Nick Comer <nick@comer.io>
2013-08-21 20:57:22 -0400

    complete error language file

--- app/lang/en/js_lang.php
+++ app/lang/en/js_lang.php
@@ -1,13 +1,39 @@
 <?php
 
 return array(
-	'session_not_found'	=> 'Session was not found. It may have expired.',
-	'session_full' 		=> 'This session is full',
-	'global_ajax_error'	=> 'Failed to communicate with server',
-	'outdated_browser'  => 'Please upgrade or install the latest version of Chrome or Firefox',
-	'need_camera_mic'	=> 'Your Second Phone needs access to your camera and microphone',
-	'socket_disconnect' => 'Connection to the chat server was lost',
-	'socket_reconnect'  => 'Chat server was reconnected',
-	'socket_connect_failed' => 'Failed to connect to the chat server :[',
-	'other_party_left'  => 'The other person left the chat'
+    'errors' => array(
+        'session_not_found' => 'Session was not found. It may have expired.',
+        'global_ajax_error' => 'Failed to communicate with server.',
+        'outdated_browser' => 'Please upgrade or install the latest version of Chrome or Firefox',
+        'dont_block_ads' => 'Please don\'t block the ads.',
+        'session_retrieval_error' => 'Failed to retrieve session data from server.',
+        'general_socket_error' => 'An unknown error occured with the chat server.',
+        'chat_connection_failed' => 'Failed to connect to chat server.',
+        'tb_load_failed' => 'Video Chat: Failed To Load',
+        'tb_auth_error' => 'Video Chat: Authentication error',
+        'tb_invalid_session_id' => 'Video Chat: Invalid Session ID',
+        'tb_connect_failed' => 'Video Chat: Connect Failed',
+        'tb_connect_rejected' => 'Video Chat: Connect Rejected',
+        'tb_connect_timeout' => 'Video Chat: Connect Time-out',
+        'tb_security_error' => 'Video Chat: Security Error',
+        'tb_not_connected' => 'Video Chat: Not Connected',
+        'tb_invalid_param' => 'Video Chat: Invalid Parameter',
+        'tb_connection_failed' => 'Video Chat: Connection Failed',
+        'tb_api_response_failed' => 'Video Chat: API Response Failure',
+        'tb_unable_to_publish' => 'Video Chat: Unable to Publish',
+        'tb_unable_to_signal' => 'Video Chat: Unable to Signal',
+        'tb_unable_to_force_disconnect' => 'Video Chat: Unable to Force Disconnect',
+        'tb_unable_to_force_unpublish' => 'Video Chat: Unable to Force Unpublish',
+        'tb_force_unpublish_on_invalid_stream' => 'Video Chat: Force Unpublish on Invalid Stream',
+        'tb_internal_error' => 'Internal Error',
+        'tb_report_issue_failure' => Report Issue Failure
+    ),
+    'session_full' => 'This session is full',
+    'global_ajax_error' => 'Failed to communicate with server',
+    'outdated_browser' => 'Please upgrade or install the latest version of Chrome or Firefox',
+    'need_camera_mic' => 'Your Second Phone needs access to your camera and microphone',
+    'socket_disconnect' => 'Connection to the chat server was lost',
+    'socket_reconnect' => 'Chat server was reconnected',
+    'socket_connect_failed' => 'Failed to connect to the chat server :[',
+    'other_party_left' => 'The other person left the chat'
 );
