dace702223ba5fcc484d30a61cba2e5b206d00d9
Nick Comer <nick@comer.io>
2013-08-21 23:56:35 -0400

    add var

--- html/static/js/src/BaseException.js
+++ html/static/js/src/BaseException.js
@@ -1,5 +1,5 @@
 define(['stapes'], function(Stapes){
-	BaseException = Stapes.subclass({
+	var BaseException = Stapes.subclass({
 		constructor: function(code, msg) {
 			this.message = msg || ((ysp_errors.hasOwnProperty(code)) ? ysp_errors[code] : ysp_lang.errors.unknown_error);
 			this.code = code;
