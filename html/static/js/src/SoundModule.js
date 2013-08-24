47baee1888a099ee885d49bb6f6c5cacb6ce9130
Nick Comer <nick@comer.io>
2013-08-24 15:01:42 -0400

    progress on sound module

--- html/static/js/src/SoundModule.js
+++ html/static/js/src/SoundModule.js
@@ -1,9 +1,26 @@
 define(['BaseModule', 'BaseException'], function(BaseModule, BaseException){
+	var join, leave, error;
+
+	join = new Audio('/static/audio/join.mp3');
+
 	var SoundModule = BaseModule.subclass({
 		module_id:"SoundModule",
 		constructor: function(callback){
 			this.init();
+
 			callback(null);
+		},
+		event: function(eventName){
+			switch(eventName){
+				case "join":
+					break;
+				case "leave":
+					break;
+				case "message":
+					break;
+				case "error":
+					break;
+			}
 		}
 	});
 
